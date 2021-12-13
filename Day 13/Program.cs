51b08eb65ae108080520fe8eb8a502eb07d4ba78
Ilya Komlyakov <ilyakom1929@gmail.com>
2021-12-13 12:11:10 +0100

    Tabify

--- Day 13/Program.cs
+++ Day 13/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 Stars();
 
 void Stars()
@@ -8,7 +7,7 @@ void Stars()
 	var points = new List<Point>();
 	var folds = new List<string[]>();
 
-    using (var reader = File.OpenRead("./input.txt"))
+	using (var reader = File.OpenRead("./input.txt"))
 	{
 		using (var s = new StreamReader(reader))
 		{
@@ -17,14 +16,14 @@ void Stars()
 				var line = s.ReadLine();
 
 				if (!string.IsNullOrWhiteSpace(line))
-                {
+				{
 					var arr = line.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
 					points.Add(new Point(arr[0], arr[1]));
-                }
+				}
 				else
-                {
+				{
 					break;
-                }
+				}
 			}
 
 			while (!s.EndOfStream)
@@ -36,37 +35,37 @@ void Stars()
 		}
 	}
 
-    foreach (var fold in folds)
-    {
+	foreach (var fold in folds)
+	{
 		var axis = fold[0];
 		var axisVal = int.Parse(fold[1]);
 
-        foreach (var point in points)
-        {
+		foreach (var point in points)
+		{
 			if (axis == "y")
-            {
+			{
 				if (point.y > axisVal)
-                {
+				{
 					point.y = axisVal - (point.y - axisVal);
-                }
-            }
+				}
+			}
 			else
-            {
+			{
 				if (point.x > axisVal)
 				{
 					point.x = axisVal - (point.x - axisVal);
 				}
 			}
-        }
-    }
+		}
+	}
 
 	var hs = new HashSet<Point>();
 
 	// STAR ONE (BUT FOR ALL FOLDS)
 	foreach(var p in points)
-    {
+	{
 		hs.Add(p);
-    }
+	}
 
 	Console.WriteLine(hs.Count);
 
@@ -74,25 +73,25 @@ void Stars()
 	var pts = new bool[hs.Max(x => x.x)+1,hs.Max(y => y.y)+1];
 
 	foreach(var point in points)
-    {
+	{
 		pts[point.x, point.y] = true;
-    }
+	}
 
 	for(int i = 0; i < hs.Max(y => y.y)+1; i++)
-    {
-        for (int j = 0; j < hs.Max(x => x.x)+1; j++)
-        {
+	{
+		for (int j = 0; j < hs.Max(x => x.x)+1; j++)
+		{
 			if (pts[j,i])
-            {
+			{
 				Console.Write("#");
-            }
-            else
-            {
+			}
+			else
+			{
 				Console.Write(".");
-            }
-        }
+			}
+		}
 		Console.WriteLine();
-    }
+	}
 }
 
 class Point
@@ -100,19 +99,19 @@ class Point
 	public int x;
 	public int y;
 
-    public Point(int x, int y)
-    {
+	public Point(int x, int y)
+	{
 		this.x = x;
 		this.y = y;
-    }
+	}
 
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(x, y);
-    }
+	public override int GetHashCode()
+	{
+		return HashCode.Combine(x, y);
+	}
 
-    public override bool Equals(object? obj)
-    {
-        return obj is Point pt && pt.x == x && pt.y == y;
-    }
+	public override bool Equals(object? obj)
+	{
+		return obj is Point pt && pt.x == x && pt.y == y;
+	}
 }
