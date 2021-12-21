0e0cbf1491ec14cb925d88b1d2d1de6ca41899b3
Ilya Komlyakov <ilyakom1929@gmail.com>
2021-12-21 11:20:07 +0100

    Day 21

---
+++ Day 17/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+// target area: x=111..161, y=-154..-101
+
+StarOne();
+StarTwo();
+
+void StarOne()
+{
+	Console.WriteLine((153 + 1) * 153 / 2);
+}
+
+void StarTwo()
+{
+    var area = (x1: 111, x2: 161, y1: -101 , y2: -154);
+    var res = 0;
+    
+    for (var h = area.x2 + 1; h >= 1; h--)
+    {
+        for (var v = area.y2; v <= area.y2 * -2 + 1; v++)
+        {
+            var path = GetSpeeds(h, 0)
+                .Zip(GetSpeeds(v, null))
+                .TakeWhile(x => (x.First.dx > 0 || x.First.x >= area.x1) && x.First.x <= area.x2 && x.Second.x >= area.y2);
+
+            if (path.Any(x => x.First.x >= area.x1 && x.Second.x <= area.y1))
+            {
+                res++;
+            }
+        }
+    }
+
+    Console.WriteLine(res);
+}
+
+IEnumerable<(int x, int dx)> GetSpeeds(int dx, int? minAcc)
+{
+    int x = 0;
+
+    while (true)
+    {
+        x += dx;
+        dx -= dx == minAcc ? 0 : 1;
+
+        yield return (x, dx);
+    }
+}
---
+++ Day 21/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+StarOne();
+
+void StarOne()
+{
+    var p1 = 4;
+    var p2 = 3;
+
+    var wins = GetWins(p1, p2, 0, 0, true);
+
+    Console.WriteLine(Math.Max(wins.Item1, wins.Item2)  + Environment.NewLine + 444356092776315);
+}
+
+(long, long) GetWins(int p1, int p2, long ts1, long ts2, bool isFPTurn)
+{
+    if (ts1 >= 21)
+    {
+        return (1, 0);
+    }
+
+    if (ts2 >= 21)
+    {
+        return (0, 1);
+    }
+
+    long resP1 = 0;
+    long resP2 = 0;
+    var dic = new Dictionary<int, (long, long)>(5);
+
+    if (isFPTurn)
+    {
+        for (var i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                for (int k = 1; k <= 3; k++)
+                {
+                    var sum = i + j + k;
+
+                    if (dic.TryGetValue(sum, out var res))
+                    {
+                        resP1 += res.Item1;
+                        resP2 += res.Item2;
+                        continue;
+                    }
+
+                    var save = p1;
+                    p1 = (p1 + i + j + k) == 10 ? 10 : ((p1 + i + j + k) % 10);
+                    ts1 += p1;
+
+                    res = GetWins(p1, p2, ts1, ts2, false);
+
+                    dic.Add(sum, res);
+
+                    resP1 += res.Item1;
+                    resP2 += res.Item2;
+
+                    ts1 -= p1;
+                    p1 = save;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (var i = 1; i <= 3; i++)
+        {
+            for (int j = 1; j <= 3; j++)
+            {
+                for (int k = 1; k <= 3; k++)
+                {
+                    var sum = i + j + k;
+
+                    if (dic.TryGetValue(sum, out var res))
+                    {
+                        resP1 += res.Item1;
+                        resP2 += res.Item2;
+                        continue;
+                    }
+
+                    var save = p2;
+                    p2 = (p2 + i + j + k)  == 10 ? 10 : ((p2 + i + j + k) % 10);
+                    ts2 += p2;
+
+                    res = GetWins(p1, p2, ts1, ts2, true);
+
+                    dic.Add(sum, res);
+
+                    resP1 += res.Item1;
+                    resP2 += res.Item2;
+
+                    ts2 -= p2;
+                    p2 = save;
+                }
+            }
+        }
+    }
+    
+    return (resP1, resP2);
+}
